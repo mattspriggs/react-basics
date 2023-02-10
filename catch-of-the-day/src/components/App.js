69b0878b55e3228a9f8dd4d2d2032385f13501d7
Matt Spriggs <mspriggs1@cnm.edu>
2023-02-10 16:04:24 +1300

    Introducing the idea of props

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -6,13 +6,17 @@ import Inventory from "./Inventory";
 class App extends React.Component {
   render() {
     return (
-      <div className="catch-of-the-day">
-        <div className="menu">
-          <Header />
+      <React.StrictMode>
+        <div className="catch-of-the-day">
+          <div className="menu">
+            {/*The below component is being passed data within it's tag, such as tagline, that allows for the
+             information to be dynamic*/}
+            <Header tagline={"Fresh Seafood Market"} age={500} />
+          </div>
+          <Order />
+          <Inventory />
         </div>
-        <Order />
-        <Inventory />
-      </div>
+      </React.StrictMode>
     );
   }
 }
