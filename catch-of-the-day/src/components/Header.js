1980e56da15d787a9ae3759b9eb7007c1a0823d3
Matt Spriggs <mspriggs1@cnm.edu>
2023-02-10 16:36:59 +1300

    Refactored the Header component to be a stateless component (in arrow function style)

--- catch-of-the-day/src/components/Header.js
+++ catch-of-the-day/src/components/Header.js
@@ -1,26 +1,44 @@
 import React from "react";
 
-class Header extends React.Component {
-  render() {
-    return (
-      <>
-        <header className="top">
-          <h1>
-            Catch
-            <span className="ofThe">
-              <span className="of"> Of</span>
-              <span className="the">The</span>
-            </span>
-            Day
-          </h1>
-          <h3 className="tagline">
-            {/*The this. below refers to the instance of the Header component*/}
-            <span>{this.props.tagline}</span>
-          </h3>
-        </header>
-      </>
-    );
-  }
-}
+// Below is a stateless component
+const Header = (props) => (
+  <header className="top">
+    <h1>
+      Catch
+      <span className="ofThe">
+        <span className="of"> Of</span>
+        <span className="the">The</span>
+      </span>
+      Day
+    </h1>
+    <h3 className="tagline">
+      {/*The this. below refers to the instance of the Header component*/}
+      <span>{props.tagline}</span>
+    </h3>
+  </header>
+);
+// The below is a full component with state (memory) attached to it
+// class Header extends React.Component {
+//   render() {
+//     return (
+//       <>
+//         <header className="top">
+//           <h1>
+//             Catch
+//             <span className="ofThe">
+//               <span className="of"> Of</span>
+//               <span className="the">The</span>
+//             </span>
+//             Day
+//           </h1>
+//           <h3 className="tagline">
+//             {/*The this. below refers to the instance of the Header component*/}
+//             <span>{this.props.tagline}</span>
+//           </h3>
+//         </header>
+//       </>
+//     );
+//   }
+// }
 
 export default Header;
