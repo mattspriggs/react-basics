92af8c9e8767d7bf82fddc45c654a10e91ae3571
Matt Spriggs <mspriggs1@cnm.edu>
2023-02-10 14:39:19 +1300

    React Fragment information

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -3,10 +3,18 @@ import React from "react";
 class StorePicker extends React.Component {
 	render() {
 		// return React.createElement('p',{className: 'hey'}, 'Heyoooo') this is NON JSX
-		return (
-			<form className="store-selector">
-				<h2>Please Enter A Store</h2>
-			</form>
+		// return (//cannot return children
+		// 	<form className="store-selector">
+		// 		<h2>Please Enter A Store</h2>
+		// 	</form>
+		// )
+		return (//The <> </> is the equivalent of <React.Fragment> </React.Fragment>
+			<>
+				<p>Fish!</p>
+				<form className="store-selector">
+					<h2>Please Enter A Store</h2>
+				</form>
+			</>
 		)
 	}
 }
