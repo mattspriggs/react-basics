34c78cf891227516ee3a18df35b73e2ea302e07f
Matt Spriggs <mspriggs1@cnm.edu>
2023-02-10 15:38:58 +1300

    Completed the Header component

--- catch-of-the-day/src/components/Header.js
+++ catch-of-the-day/src/components/Header.js
@@ -5,7 +5,14 @@ class Header extends React.Component {
     return (
       <>
         <header className="top">
-          <h1>Catch of the Day</h1>
+          <h1>
+            Catch
+            <span className="ofThe">
+              <span className="of"> Of</span>
+              <span className="the">The</span>
+            </span>
+            Day
+          </h1>
           <h3 className="tagline">
             <span>Fresh Daily</span>
           </h3>
