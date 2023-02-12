015d3c30bdc15df2db819de00f70524b157ae259
Matt Spriggs <mspriggs1@cnm.edu>
2023-02-13 11:35:48 +1300

    Working on events in React

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -1,24 +1,35 @@
-import React from 'react';
+import React from "react";
+import { getFunName } from "../helpers";
 
 class StorePicker extends React.Component {
-	render() {
-		// return React.createElement('p',{className: 'hey'}, 'Heyoooo') this is NON JSX
-		// return (//cannot return children
-		// 	<form className="store-selector">
-		// 		<h2>Please Enter A Store</h2>
-		// 	</form>
-		// )
-		return (//The <> </> is the equivalent of <React.Fragment> </React.Fragment>
-			<>
-				<form className="store-selector">
-					{/* comment in JSX and has to be this way when inside the returned element that is in JSX*/}
-					<h2>Please Enter A Store</h2>
-					<input type={"text"} required placeholder={"Store Name"}/>
-					<button type={"submit"}>Visit Store ➡️</button>
-				</form>
-			</>
-		)
-	}
+  goToStore(event) {
+    event.preventDefault();
+    console.log("Going to store....");
+  }
+  render() {
+    // return React.createElement('p',{className: 'hey'}, 'Heyoooo') this is NON JSX
+    // return (//cannot return children
+    // 	<form className="store-selector">
+    // 		<h2>Please Enter A Store</h2>
+    // 	</form>
+    // )
+    return (
+      //The <> </> is the equivalent of <React.Fragment> </React.Fragment>
+      <>
+        <form className="store-selector" onSubmit={this.goToStore}>
+          {/* comment in JSX and has to be this way when inside the returned element that is in JSX*/}
+          <h2>Please Enter A Store</h2>
+          <input
+            type={"text"}
+            required
+            placeholder={"Store Name"}
+            defaultValue={getFunName()}
+          />
+          <button type={"submit"}>Visit Store ➡️</button>
+        </form>
+      </>
+    );
+  }
 }
 
-export default StorePicker
+export default StorePicker;
