68197092c182a41f7ed18a8f3cb25fa10d51d9b8
Matt Spriggs <mspriggs1@cnm.edu>
2023-02-13 09:57:23 +1300

    Troubleshooting router for react

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -6,17 +6,15 @@ import Inventory from "./Inventory";
 class App extends React.Component {
   render() {
     return (
-      <React.StrictMode>
-        <div className="catch-of-the-day">
-          <div className="menu">
-            {/*The below component is being passed data within it's tag, such as tagline, that allows for the
+      <div className="catch-of-the-day">
+        <div className="menu">
+          {/*The below component is being passed data within it's tag, such as tagline, that allows for the
              information to be dynamic*/}
-            <Header tagline={"Fresh Seafood Market"} />
-          </div>
-          <Order />
-          <Inventory />
+          <Header tagline={"Fresh Seafood Market"} />
         </div>
-      </React.StrictMode>
+        <Order />
+        <Inventory />
+      </div>
     );
   }
 }
