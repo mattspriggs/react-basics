5599b00da237497e90430c1c768a1abdef4bd1cc
Matt Spriggs <mspriggs1@cnm.edu>
2023-02-13 15:56:21 +1300

    Working on the invetory form

--- catch-of-the-day/src/components/AddFishForm.js
+++ catch-of-the-day/src/components/AddFishForm.js
@@ -1,11 +1,23 @@
 import React from "react";
 
 class AddFishForm extends React.Component {
+  createFish = (event) => {
+    event.preventDefault();
+    console.log("Making a fish");
+  };
   render() {
     return (
       <>
-        <form>
-          <p>It works</p>
+        <form className="fish-edit" onSubmit={this.createFish}>
+          <input name="name" type="text" placeholder="Name" />
+          <input name="price" type="text" placeholder="Price" />
+          <select name="status">
+            <option value="available">Fresh!</option>
+            <option value="unavailable">Sold-Out</option>
+          </select>
+          <textarea name="desc" placeholder="Desc" />
+          <input name="image" type="text" placeholder="Image" />
+          <button type="submit">+ Add Fish</button>
         </form>
       </>
     );
