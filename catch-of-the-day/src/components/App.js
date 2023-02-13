b753913651cd8f503bfd19a7b2efbc5fa9b433d0
Matt Spriggs <mspriggs1@cnm.edu>
2023-02-13 16:58:53 +1300

    Added the ability to load the state with sample fish

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import Header from "./Header";
 import Order from "./Order";
 import Inventory from "./Inventory";
+import sampleFishes from "../sample-fishes";
 
 class App extends React.Component {
   state = {
@@ -19,6 +20,9 @@ class App extends React.Component {
       fishes,
     });
   };
+  loadSampleFishes = () => {
+    this.setState({ fishes: sampleFishes });
+  };
   render() {
     return (
       <div className="catch-of-the-day">
@@ -28,7 +32,7 @@ class App extends React.Component {
           <Header tagline={"Fresh Seafood Market"} />
         </div>
         <Order />
-        <Inventory addFish={this.addFish} />
+        <Inventory addFish={this.addFish} loadSamples={this.loadSampleFishes} />
       </div>
     );
   }
