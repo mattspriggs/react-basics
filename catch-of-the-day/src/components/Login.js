1b39adbb07e1204273f770be7bcdbb0ebc687ac3
Matt Spriggs <mspriggs1@cnm.edu>
2023-02-17 15:54:55 +1300

    Debugged the login function

--- catch-of-the-day/src/components/Login.js
+++ catch-of-the-day/src/components/Login.js
@@ -4,14 +4,14 @@ import PropTypes from "prop-types";
 const Login = (props) => (
   <nav className="login">
     <h2>Inventory Login</h2>
-    <p>Please sign in to manage your store's inventory</p>
-    <button className={"github"} onClick={() => props.authenticate("Github")}>
-      Log In With Github
+    <p>Sign in to manage your store's inventory.</p>
+    <button className="github" onClick={() => props.authenticate("Github")}>
+      Log In With GitHub
     </button>
-    <button
-      className={"facebook"}
-      onClick={() => props.authenticate("Facebook")}
-    >
+    <button className="twitter" onClick={() => props.authenticate("Twitter")}>
+      Log In With Twitter
+    </button>
+    <button className="facebook" onClick={() => props.authenticate("Facebook")}>
       Log In With Facebook
     </button>
   </nav>
@@ -20,4 +20,5 @@ const Login = (props) => (
 Login.propTypes = {
   authenticate: PropTypes.func.isRequired,
 };
+
 export default Login;
