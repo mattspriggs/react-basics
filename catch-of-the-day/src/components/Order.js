9e7b6124bd3b3f89d2c53f6adccd359edb618fc5
Matt Spriggs <mspriggs1@cnm.edu>
2023-02-21 16:40:30 +1300

    fixed the running item total to align with the full order total

--- catch-of-the-day/src/components/Order.js
+++ catch-of-the-day/src/components/Order.js
@@ -43,13 +43,11 @@ class Order extends React.Component {
               </CSSTransition>
             </TransitionGroup>
             lbs {fish.name}{" "}
-            <span className="priceStyle">
-              {formatPrice(count * fish.price)}
-            </span>
-            <button onClick={() => this.props.removeFromOrder(key)}>
-              &times;
-            </button>
           </span>
+          {formatPrice(count * fish.price)}
+          <button onClick={() => this.props.removeFromOrder(key)}>
+            &times;
+          </button>
         </li>
       </CSSTransition>
     );
